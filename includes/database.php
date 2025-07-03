4a1f61c7daa2cb0f6a08b934138331974c1fb038
Alex Sullon <alexsullonporras@hotmail.com>
2025-07-03 08:02:21 -0500

    fix-01: Se corrigió el error de NOMBRE_PROYECTO

--- includes/database.php
+++ includes/database.php
@@ -1,3 +1,3 @@
 <?php
 
-$db = new PDO("mysql:host=" . $_ENV['DB_HOST'] .";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USR'], $_ENV['DB_PASS']);
+$db = new PDO("mysql:host=" . $_ENV['DB_HOST'] .";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
--- views/servicios/index.php
+++ views/servicios/index.php
@@ -2,7 +2,7 @@
     <section class="servicios mt-2">
         <h1>Servicios</h1>
         <p class="mt-2">
-            Sabemos cuánto amas a tu mascota, por eso en <strong><?php echo NOMBRE_PROYECTO ?></strong> te ofrecemos atención con cariño y compromiso. Contamos con servicios de consulta veterinaria, vacunación, baño y peluquería, todo pensado para su salud, bienestar y alegría. Porque su felicidad también es la nuestra.
+            Sabemos cuánto amas a tu mascota, por eso en <strong><?php echo $_ENV['APP_NAME'] ?></strong> te ofrecemos atención con cariño y compromiso. Contamos con servicios de consulta veterinaria, vacunación, baño y peluquería, todo pensado para su salud, bienestar y alegría. Porque su felicidad también es la nuestra.
         </p>
         <div class="servicios__contenido mt-2">
             <?php foreach ($servicios as $servicio): ?>
