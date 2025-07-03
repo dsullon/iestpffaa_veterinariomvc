4ffbd98c4ceef3e4492a031541ef66299359b95f
Alex Sullon <alexsullonporras@hotmail.com>
2025-07-03 14:41:02 -0500

    Se agregó estilos a la página de inicio

--- views/home/404.php
+++ views/home/404.php
@@ -1,2 +1,6 @@
-<h1>Página no encontrada</h1>
-<p>Verifique la url de la solicitud.</p>
+<main class="contenedor">
+    <div class="error mt-2">
+        <h1>Página no encontrada</h1>
+        <p>Verifique la url de la solicitud.</p>
+    </div>
+</main>
--- views/home/index.php
+++ views/home/index.php
@@ -1,1 +1,5 @@
-<h1>Pagina de inicio</h1>
+<main class="contenedor">
+    <div class="principal mt-2">
+        <h1>Pagina de inicio</h1>
+    </div>
+</main>
